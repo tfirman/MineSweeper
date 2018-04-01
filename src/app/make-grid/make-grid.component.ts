e598f02bc38a9dd3087a37c14bdb50abecf58f68
Tim Firman <tkfirman@gmail.com>
2018-04-01 12:38:24 -0700

    attempts input acceptance, not yet fnctional

--- src/app/make-grid/make-grid.component.ts
+++ src/app/make-grid/make-grid.component.ts
@@ -8,13 +8,15 @@ import { Square } from '../models/square.model';
 })
 export class MakeGridComponent {
   @Input() childGridList: Square[];
+  @Input() childSelectedSquare: Square[];
   @Output() clickSender = new EventEmitter();
 
   squareClicked(squareToEdit: Square) {
     this.clickSender.emit(squareToEdit);
   }
+
 onKeyDown(event){
-  
+
 }
 
 }
